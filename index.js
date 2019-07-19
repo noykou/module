aee11c0d4f081ca31ae0d72695f077bf86cab21e
Androz <androz2091@gmail.com>
2019-07-19 22:13:31 +0200

    Encode the url

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const baseURL = "https://noykou.glitch.me/api/v1/";
 module.exports = {
 	async getImage(endpoint, options){
 		return new Promise(async(resolve, reject) => {
-			const { body } = await get(`${baseURL}${endpoint}?username=${options.username.toLowerCase())}&url=${options.url}&text=${options.text}&message=${options.message}&back=${options.back}&style=${options.style}&roundcolor=${options.roundcolor}&textcolor=${options.textcolor}`)
+			const { body } = await get(encodeURI(`${baseURL}${endpoint}?username=${options.username.toLowerCase())}&url=${options.url}&text=${options.text}&message=${options.message}&back=${options.back}&style=${options.style}&roundcolor=${options.roundcolor}&textcolor=${options.textcolor}`));
 			resolve(body);
 		});
 	}
